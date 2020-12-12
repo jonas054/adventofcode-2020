66210b4a1122b8c811c9ef73da8a3044c3cac702
Jonas Arvidsson <jonas054@gmail.com>
2020-12-12 13:34:50 +0100

    Day 11 completed

---
+++ 11.rb
@@ -0,0 +1,46 @@
+# input = <<~INPUT
+#   L.LL.LL.LL
+#   LLLLLLL.LL
+#   L.L.L..L..
+#   LLLL.LL.LL
+#   L.LL.LL.LL
+#   L.LLLLL.LL
+#   ..L.L.....
+#   LLLLLLLLLL
+#   L.LLLLLL.L
+#   L.LLLLL.LL
+# INPUT
+
+input = File.read('11.txt')
+
+DIRECTIONS = [-1, 0, 1].repeated_permutation(2).to_a - [[0, 0]]
+
+def apply_rules(rows)
+  result = rows.map(&:dup)
+  rows.each_with_index do |row, row_ix|
+    row.chars.each_with_index do |seat, col_ix|
+      occupied_count = DIRECTIONS.count { |dx, dy| occupied?(rows, row_ix + dy, col_ix + dx) }
+      if seat == 'L' && occupied_count == 0
+        result[row_ix][col_ix] = '#'
+      elsif seat == '#' && occupied_count >= 4
+        result[row_ix][col_ix] = 'L'
+      end
+    end
+  end
+  result
+end
+
+def occupied?(rows, y, x)
+  (0...rows.size).include?(y) && (0...rows[y].size).include?(x) && rows[y][x] == '#'
+end
+
+current_state = input.lines
+loop do
+  next_state = apply_rules(current_state)
+  break if next_state == current_state
+
+  current_state = next_state
+  info current_state.join
+end
+
+debug current_state.join.count('#')
--- Rakefile
+++ Rakefile
@@ -1,4 +1,4 @@
 task :default do
-  ruby '-I. -rrainbow -rdebugging 10b.rb'
-  sh 'rubocop -A --disable-uncorrectable 10*.rb'
+  sh 'rubocop -A --disable-uncorrectable 11*.rb'
+  ruby '-I. -rrainbow -rdebugging 11b.rb'
 end
